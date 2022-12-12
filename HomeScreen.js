8b8432bab5083a6e2180613ca8b2902f970a62b4
DavidKuonen <david.kuonen@gmail.com>
2022-12-12 16:03:08 +0200

    Semi working Prototype

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,47 +1,99 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
-import React, { useState} from 'react';
+import { StyleSheet, Text, View, Button, FlatList } from 'react-native';
+import React, { useState, useEffect } from 'react';
 import { initializeApp } from 'firebase/app';
 import { getDatabase, push, ref, onValue, set } from 'firebase/database';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBeOOEdabk5jsDIMv-mprLtBK1Rlg70Kbw", 
+    apiKey: "AIzaSyBeOOEdabk5jsDIMv-mprLtBK1Rlg70Kbw",
     authDomain: "restaurantappdavidkuonen.firebaseapp.com",
     databaseURL: "https://restaurantappdavidkuonen-default-rtdb.europe-west1.firebasedatabase.app",
     projectId: "restaurantappdavidkuonen",
     storageBucket: "restaurantappdavidkuonen.appspot.com",
-    messagingSenderId: "26895683017", 
+    messagingSenderId: "26895683017",
     appId: "1:26895683017:web:b84495461a1ac7fb456c18"
-  };
-  
-  const app = initializeApp(firebaseConfig);
-  const database = getDatabase(app);
-  
-  function writeRestaurantData(restaurantId, name, address){
-    const reference = ref(database,'restaurants/'+restaurantId);
+};
 
-    set(reference,{
-        restaurantName : name,
-        restaurantAddress : address
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+function writeRestaurantData(restaurantId, name, address) {
+    const reference = ref(database, 'restaurants/' + restaurantId);
+
+    set(reference, {
+        restaurantName: name,
+        restaurantAddress: address,
+        menu: {
+            0: {
+                name: 'Chicken Burger',
+                price: 4.90
+
+            },
+            1: {
+                name: 'Cheese Burger',
+                price: 3.60
+            },
+            2: {
+                name: 'Vegan Burger',
+                price: 4.80
+            }
+        }
     });
-  }
+}
+
+
 
 export default function HomeScreen({ navigation }) {
+
+    const [restaurantName, setRestaurantName] = useState('');
+    const [restaurantAddress, setRestaurantAddress] = useState('');
+    const [restaurantMenu, setRestaurantMenu] = useState([]);
+    const [name, setName] = useState('');
+    const [price, setPrice] = useState('');
+    const [restaurants, setRestaurants] = useState([]);
+    const [keys, setKeys] = useState([]);
+
+    useEffect(() => {
+        const itemsRef = ref(database, 'restaurants/');
+        onValue(itemsRef, (snapshot) => {
+            const data = snapshot.val();
+            if (data) {
+                setRestaurants(Object.values(data));
+                setKeys(Object.keys(data));
+            }
+            else {
+                setRestaurants([]);
+            }
+        })
+
+    }, []);
+
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Hello screen</Text>
-            
-            <Button
-                title="Restaurant"
-                onPress={() => navigation.navigate('Restaurant')} // Navigate to Restaurant screen
-                //How to pass parameter example:
-                //<Button title='History' onPress={() => navigation.navigate('History', {param: results})}></Button>
+
+            <FlatList
+                data={restaurants}
+
+                renderItem={({ item}) => (
+
+                    <View>
+                        <Text>{item.restaurantName}, {item.restaurantAddress} </Text>
+                        <Button
+                            //How to pass parameter example:
+                            //<Button title='History' onPress={() => navigation.navigate('History', {param: results})}></Button>
+                            title="Choose" onPress={() => navigation.navigate('Restaurant', { restName: item.restaurantName,
+                            restAddress: item.restaurantAddress })}> </Button>
+                        <Text>{item.menu[0].name}, {item.menu[0].price} Euro</Text>
+                        <Text>{item.menu[1].name}, {item.menu[1].price} Euro</Text>
+                        <Text>{item.menu[2].name}, {item.menu[2].price} Euro</Text>
+                        <Text>-----------------------------------------</Text>
+                        
+
+                    </View>
+
+                )}
+
             />
-            <Button
-                title="Upload"
-                onPress={writeRestaurantData("2","Fafa","Mall of Tripla")} // Navigate to Restaurant screen
-                //How to pass parameter example:
-                //<Button title='History' onPress={() => navigation.navigate('History', {param: results})}></Button>
-            />
+
 
         </View>
     );
--- RestaurantScreen.js
+++ RestaurantScreen.js
@@ -1,17 +1,86 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
-import React, { useState} from 'react';
+import { StyleSheet, Text, TextInput, View, Button, FlatList } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import MapView, { Marker } from 'react-native-maps';
 
-export default function RestaurantScreen({ navigation }) {
+export default function RestaurantScreen({ route, navigation }) {
 
-    //const{ restaurant } = route.params;
-    
+    const { restName, restAddress, menu } = route.params;
+
+    const {restaurantName, setRestaurantName} = useState(JSON.stringify(restName));
+    const {restaurantAddress, setRestaurantAddress} = useState(JSON.stringify(restAddress));
+    const {restaurantMenu, setRestauranMenu} = useState([]);
+
+
+    const key = "C0r8kKdwsm6ea23nPoIQ4FIeVqvWSlZX";
+
+    const [region, setRegion] = useState({
+        latitude: 60.200692,
+        longitude: 24.934302,
+        latitudeDelta: 0.0322,
+        longitudeDelta: 0.0221,
+    });
+    const [marker, setMarker] = useState({
+        latitude: 60.201373,
+        longitude: 24.934041
+    });
+
+    const [input, setInput] = useState("Mall of Tripla, Helsinki");
+
+    const fetchLocation = () => {
+        fetch('https://www.mapquestapi.com/geocoding/v1/address?key=' + key + '&location=' + input)
+            .then(response => response.json())
+            .then(data => {
+                location = data.results[0].locations[0]
+
+                setRegion({
+                    latitude: location.latLng.lat,
+                    longitude: location.latLng.lng,
+                    latitudeDelta: 0.0322,
+                    longitudeDelta: 0.0221
+                });
+
+                setMarker({
+                    latitude: location.latLng.lat,
+                    longitude: location.latLng.lng,
+                })
+            })
+            .catch(error => {
+                Alert.alert('Error', error);
+            });
+    }
+
+    useEffect(() => {
+        //Change to the Address of chosen Restaurant from previous screen with help of param      
+        setInput(JSON.stringify(restAddress));
+        fetchLocation();
+    })
+        ;
+
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Hello screen</Text>
-            <Button
-                title="Home"
-                onPress={() => navigation.navigate('Home')} // Navigate to Home screen
+            <MapView
+                style={{ flex: 1, height: '50%', width: '100%' }}
+                region={region}>
+                <Marker
+                    coordinate={marker}
+                    title={JSON.stringify(restName)} />
+            </MapView>
+            <TextInput
+                placeholderTextColor='grey'
+                placeholder='Location'
+                onChangeText={text => setInput(text)}
             />
+            <Text> Blabla , {JSON.stringify(restAddress)}</Text>
+
+            <View>
+
+                <Button
+                    title="Home"
+                    onPress={() => navigation.navigate('Home')} // Navigate to Home screen
+                />
+
+            </View>
+
         </View>
     );
 }
